61b799338253cb8517d12728d9115cb09492c918
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-04-22 00:37:29 +0700

    feat: try spawning vm using incus

--- infra/incus/init.sh
+++ infra/incus/init.sh
@@ -1,6 +1,5 @@
-sudo incus launch images:ubuntu/22.04/cloud vm-cloud \
+sudo incus launch images:ubuntu/22.04/cloud master \
     --vm \
     -c limits.cpu=4 \
     -c limits.memory=8GB \
-    -c security.agent.certified=true \
     -c user.user-data="$(cat cloud-init.yaml)"
