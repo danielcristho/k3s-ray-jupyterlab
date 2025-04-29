17f9b796eb270ddc30f756309b7b858070ea70b1
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-04-29 20:37:52 +0700

    feat: create new docker image

--- rpl/infra/ssh_jumper.sh
+++ rpl/infra/ssh_jumper.sh
@@ -1,1 +1,5 @@
-ssh -J daniel@10.21.73.122 ray@192.168.122.50 -L 8265:localhost:8265
+ssh -J daniel@10.21.73.122 root@192.168.122.50 \
+  -L 10001:localhost:10001 \
+  -L 8265:localhost:8265 \
+  -L 6379:localhost:6379 \
+  -L 8080:localhost:8081
